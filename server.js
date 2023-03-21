43ed07241bb9c8e55706738a8f035ad7d2e7f369
no-yan <63000297+no-yan@users.noreply.github.com>
2023-03-21 23:05:44 +0900

    redis

--- server.js
+++ server.js
@@ -10,20 +10,37 @@ const redis = new Redis({
 });
 
 const init = async () => {
-  await Promise.all([
-    redis.set("users:1", JSON.stringify({ id: 1, name: "alpha" })),
-    redis.set("users:2", JSON.stringify({ id: 2, name: "bravo" })),
-    redis.set("users:3", JSON.stringify({ id: 3, name: "charlie" })),
-    redis.set("users:4", JSON.stringify({ id: 4, name: "delta" })),
-  ]);
+  await redis.rpush("users:list", JSON.stringify({ id: 1, name: "alpha" }));
+  await redis.rpush("users:list", JSON.stringify({ id: 2, name: "bravo" }));
+  await redis.rpush("users:list", JSON.stringify({ id: 3, name: "charlie" }));
+  await redis.rpush("users:list", JSON.stringify({ id: 4, name: "delta" }));
 };
 
 app.get("/", (req, res) => {
   res.status(200).send("hello world\n");
 });
 
-app.get("/user/:id", (req, res) => {
-  res.status(200).send(req.params.id);
+app.get("/users", async (req, res) => {
+  const offset = req.query.offset ? Number(req.query.offset) : 0;
+  const usersList = await redis.lrange("users:list", offset, offset + 1);
+
+  const users = usersList.map((user) => {
+    return JSON.parse(user);
+  });
+
+  return { users };
+});
+
+app.get("/user/:id", async (req, res) => {
+  try {
+    const key = `users:${req.params.id}`;
+    const val = await redis.get(key);
+    const user = JSON.parse(val);
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("internal error");
+  }
 });
 
 redis.once("ready", async () => {
